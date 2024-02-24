1fbfa23dca7a0140f9c8d1a2ea6163209dedf1fd
dhruvilrathod <dhruvilrathod2001@gmail.com>
2024-02-24 13:05:54 +1030

    another sample project initialized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AppComponent } from './app.component';
+import { ErrorComponent } from './components/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path:"home",
+        component: HomeComponent
+    },
+    {
+        path: "",
+        component: AppComponent
+    },
+    {
+        path: "error",
+        component: ErrorComponent
+    },
+    {
+        path: "**",
+        redirectTo: "/error"
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
